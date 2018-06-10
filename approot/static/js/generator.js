b15358f7dc4b762bb3f9b3c43222dd819d3dce71
bluejcc <vincent@bluej.cc>
2018-06-09 22:47:44 -0700

    v1.0 Support playing MIDI.

--- approot/static/js/generator.js
+++ approot/static/js/generator.js
@@ -1,19 +1,25 @@
-MIDI.loadPlugin({
-    instrument: "acoustic_grand_piano", // or the instrument code 1 (aka the default)
-    instruments: [ "acoustic_grand_piano", "acoustic_guitar_nylon" ], // or multiple instruments
-    onsuccess: function() {}
-});
+var midiUpdate = function(time) {
+    console.log(time);
+}
+var midiStop = function() {
+    $("#btn_play").removeAttr("disabled");
+}
 
 $(function () {
+    var midiOutput = "";
+
     $("#btn_go").click(function () {
+        if ($("#btn_play").prop("disabled")) {
+            $("#btn_play").removeAttr("disabled");
+            $("#player").midiPlayer.stop();
+        }
         $("#result").show();
+        $(".well_head").show();
         let chords = $("#chords").val();
         if (!chords || chords.length == 0) {
             chords = "G D Em C G";
         }
-        chords = chords.replace(" ", "+");
-        mid_url = "generate/mid?chords=" + chords;
-        MIDI.Player.loadFile(mid_url);
+        chords = chords.replace(/ /g, "+");
 
         var VexDocument = null;
         var VexFormatter = null;
@@ -22,7 +28,6 @@ $(function () {
             success: function(data) {
                 var start = new Date().getTime(); // time execution
                 VexDocument = new Vex.Flow.Document(data);
-                //console.log(VexDocument);
                 var content = $(".music_xml_viewer")[0];
                 if (VexDocument) {
                     VexFormatter = VexDocument.getFormatter();
@@ -39,13 +44,40 @@ $(function () {
                 });
             }
         });
+        mid_url = "generate/mid?chords=" + chords + "&t=" + Date.now();
+
+        let xhr = new XMLHttpRequest();
+        xhr.open("GET", mid_url);
+        xhr.overrideMimeType("text/plain; charset=x-user-defined");
+        xhr.onreadystatechange = function() {
+            if (this.readyState === 4) {
+                if (this.status === 200) {
+                    let t = this.responseText || '';
+                    let ff = []
+                    let mx = t.length;
+                    let scc = String.fromCharCode;
+					          for (let z = 0; z < mx; z++) {
+						            ff[z] = scc(t.charCodeAt(z) & 255);
+					          }
+					          let data = ff.join('');
+                    midiOutput = "data:audio/midi;base64," + window.btoa(data);
+                    $("#player").midiPlayer.play(t);
+                }
+            }
+        };
+        xhr.send();
     });
 
     $("#btn_play").click(function() {
-        MIDI.Player.start();
+        $(this).attr("disabled","disabled");
+        $("#player").midiPlayer.play(midiOutput);
     });
-    $("#btn_pause").click(function() {
-        MIDI.Player.pause();
+
+    $("#player").midiPlayer({
+        color: "#FF5600",
+        //onUpdate: midiUpdate,
+        onStop: midiStop,
+        width: 250
     });
 
 });
